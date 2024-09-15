a988ecc4d5b0d1e3ebb6e021ffeb783c1e663779
William Giammona <wgiammona@gmail.com>
2024-09-15 17:13:08 +0300

    finished image

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="flex justify-between items-center max-w-5xl h-20 mx-auto">
+      <figure className="flex-shrink-0">
+        <Image
+          className="bg-transparent mix-blend-lighten"
+          src="/assets/Logo-pic.png"
+          alt="NavBar Logo"
+          width={200}
+          height={50}
+        />
+      </figure>
+      <ul className="flex">
+        <li className="mx-8">
+          <Link href="/">Login</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li className="mx-8">
+          <Link href="/">Contact</Link>
+        </li>
+        <li>
+          <Link href="/about">Help</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
