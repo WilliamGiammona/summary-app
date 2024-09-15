fa58939933183ecb9f85c875cd0ebe0363f69256
William Giammona <wgiammona@gmail.com>
2024-09-15 17:58:04 +0300

    creating title, description, and login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div className="max-w-5xl mx-auto flex p-6">
+          <div className="w-1/2 bg-green-200">
+            <h1>Gain more knowledge in less time</h1>
+            <p>
+              Great summaries for busy people, individuals who barely have time
+              to read, and even people who don’t like to read.
+            </p>
+            <button>Login</button>
+          </div>
+          <div className="w-1/2 bg-red-200">bye</div>
+        </div>
+      </section>
+    </>
+  );
 }
