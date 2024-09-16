d0bdbf54b1c91d17fd8b70090f82a8c2650bf866
William Giammona <wgiammona@gmail.com>
2024-09-16 14:42:59 +0300

    adding footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
