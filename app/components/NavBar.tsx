bb55854dd7f85f6716600e7b1c24d986f6868134
William Giammona <wgiammona@gmail.com>
2024-09-20 14:16:39 +0300

    code refactor

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,33 +1,48 @@
-import React from "react";
-import Link from "next/link";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
+import LoginModal from "../Auth/LoginModal";
 
 const NavBar = () => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => setIsLoginModalOpen(true);
+  const closeLoginModal = () => setIsLoginModalOpen(false);
+
   return (
-    <nav className="flex justify-between items-center max-w-5xl h-20 mx-auto mb-6">
-      <figure className="flex-shrink-0">
-        <Image
-          src="/assets/Logo-pic.png"
-          alt="NavBar Logo"
-          width={230}
-          height={80}
-        />
-      </figure>
-      <ul className="flex  text-base">
-        <li className="mx-4 hover:text-nav-hover transition-colors duration-100">
-          <Link href="/">Login</Link>
-        </li>
-        <li className="mx-4 cursor-not-allowed">
-          <span>About</span>
-        </li>
-        <li className="mx-4 cursor-not-allowed">
-          <span>Contact</span>
-        </li>
-        <li className="mx-4 cursor-not-allowed">
-          <span>Help</span>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="flex justify-between items-center max-w-5xl h-20 mx-auto mb-6">
+        <figure className="flex-shrink-0">
+          <Image
+            src="/assets/Logo-pic.png"
+            alt="NavBar Logo"
+            width={230}
+            height={80}
+          />
+        </figure>
+        <ul className="flex  text-base">
+          <li className="mx-4 hover:text-nav-hover transition-colors duration-100">
+            <button
+              onClick={openLoginModal}
+              className="hover:text-nav-hover transition-colors duration-100"
+            >
+              Login
+            </button>
+          </li>
+          <li className="mx-4 cursor-not-allowed">
+            <span>About</span>
+          </li>
+          <li className="mx-4 cursor-not-allowed">
+            <span>Contact</span>
+          </li>
+          <li className="mx-4 cursor-not-allowed">
+            <span>Help</span>
+          </li>
+        </ul>
+      </nav>
+      <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
+    </>
   );
 };
 
