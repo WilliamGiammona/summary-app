8e97cf92017fedb5382fdbf3a98ffc5dee8299f9
William Giammona <wgiammona@gmail.com>
2024-10-23 17:46:18 +0300

    code refactoring

--- app/Auth/LoginModal.tsx
+++ app/Auth/LoginModal.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../../firebaseConfig";
 import {
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
